28b1d9a316922c19ee6d446c31a7e6682828a0a8
DadaLV <vynohradova.l.a@gmail.com>
2024-02-08 13:02:19 +0100

    add tests

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     name { Faker::Lorem.word }
     description { Faker::Lorem.sentence }
-    status { 'Task::STATUS.sample' }
+    status { Task::STATUS.sample }
     project
   end
 end
